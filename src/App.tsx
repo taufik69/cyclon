124fb2bec47b5f2f52cc8c78d672e1e20b0a3d43
taufik69 <taufik35-2915@diu.edu.bd>
2025-07-12 22:03:28 +0600

    product right page show done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Shop from "./pages/Shop";
+import Layout from "./components/CommonComponets/Layout";
 const queryClient = new QueryClient();
 const App = () => {
   return (
@@ -9,7 +11,11 @@ const App = () => {
       <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path=" *" element={<h1>404</h1>} key="404"></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
